259d0bc8f60a2a832396e10ba5e4b215740cf4bb
abrarCSE29 <96782774+abrarCSE29@users.noreply.github.com>
2024-07-26 19:35:50 +0600

    Get and Post method control

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 var express = require('express');
-
+var cors = require('cors');
 var app = express();
-
+var bodyparser = require('body-parser');
+app.use(cors());
+app.use(bodyparser.json());
 function rootCall(req,res){
-    res.send('Hello World!');
+    res.send('This is my first project!');
 }
-app.get('/', rootCall)
+app.get('/', rootCall);
+app.get('/fruits/banana', (req, res) => {
+    res.send('I love bananas!');
+});
+
+user = [
+    {name:'abul'},
+    {name:'kabul'},
+    {name:'sabul'}]
+app.get('/users/:id',(req, res) => {
+    res.send(user[req.params.id]);
+})
+
+
+app.post('/addUser',(req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+    
+})
 app.listen(3000, () => {console.log('listening on port 3000')});
